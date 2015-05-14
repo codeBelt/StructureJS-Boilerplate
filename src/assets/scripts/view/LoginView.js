600316bf856cd2503d5bffa31a0cdcc2db3eaa1a
codeBelt <code@codebelt.com>
2015-05-13 20:52:20 -0500

    clean up

--- src/assets/scripts/view/LoginView.js
+++ src/assets/scripts/view/LoginView.js
@@ -17,15 +17,35 @@ var LoginView = (function () {
 
     function LoginView() { // jshint ignore:line
         _super.call(this);
+
+        /**
+         * TODO: YUIDoc_comment
+         *
+         * @property TITLE_TEXT
+         * @type {string}
+         * @constant
+         */
+        this.TITLE_TEXT = 'StructureJS Boilerplate (Browserify)';
+
+        /**
+         * TODO: YUIDoc_comment
+         *
+         * @property _$loginBtn
+         * @type {jQuery}
+         * @private
+         */
+        this._$loginBtn = null;
     }
 
     /**
      * @overridden DOMElement.create
      */
     LoginView.prototype.create = function () {
-        _super.prototype.create.call(this, 'templates/login/LoginTemplate');
+        _super.prototype.create.call(this, 'templates/login/LoginTemplate', {title: this.TITLE_TEXT});
 
         // Create or setup objects in this parent class.
+
+        this._$loginBtn = this.$element.find('.js-loginBtn');
     };
 
     /**
@@ -35,6 +55,8 @@ var LoginView = (function () {
         if (this.isEnabled === true) { return this; }
 
         // Enable the child objects and/or add any event listeners.
+
+        this._$loginBtn.addEventListener('click', this._onClick, this);
 
         return _super.prototype.enable.call(this);
     };
@@ -46,6 +68,8 @@ var LoginView = (function () {
         if (this.isEnabled === false) { return this; }
 
         // Disable the child objects and/or remove any event listeners.
+
+        this._$loginBtn.removeEventListener('click', this._onClick, this);
 
         return _super.prototype.disable.call(this);
     };
@@ -67,6 +91,18 @@ var LoginView = (function () {
         // This super method will also null out your properties for garbage collection.
 
         _super.prototype.destroy.call(this);
+    };
+
+    /**
+     * TODO: YUIDoc_comment
+     *
+     * @method _onClick
+     * @private
+     */
+    LoginView.prototype._onClick = function(event) {
+        event.preventDefault();
+
+        alert('Sign In Button Clicked')
     };
 
     return LoginView;
